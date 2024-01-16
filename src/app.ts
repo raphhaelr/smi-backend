793293eb17867621296c350f9e59af51f38da603
Raphael Rodrigues <raphaelrbh7@gmail.com>
2024-01-16 14:09:10 -0300

    fix

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import { routes } from './routes'
 import cors from '@fastify/cors'
 import swagger from '@fastify/swagger'
 import swaggerUi from '@fastify/swagger-ui'
-import helmet from '@fastify/helmet'
 
 export const app = Fastify()
 
@@ -25,26 +24,6 @@ app.register(swagger, {
 app.register(cors, {
   origin: '*',
   credentials: true,
-})
-
-app.register(helmet, (instance) => {
-  return {
-    contentSecurityPolicy: {
-      directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
-        // Add more directives as needed based on your application requirements
-        // ...
-
-        // Allow 'Origin-Agent-Cluster' header
-        frameAncestors: [
-          "'self'",
-          'http://ec2-3-145-145-251.us-east-2.compute.amazonaws.com',
-        ],
-        // Include other relevant directives here
-      },
-    },
-  }
 })
 
 app.register(swaggerUi, {
